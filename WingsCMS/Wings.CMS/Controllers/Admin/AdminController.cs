4ec2ed0862d6187e4e225825e26ea875c890176b
yhwang <yahui9119@gmail.com>
2013-04-17 23:59:22 +0800

    一个后台模版的使用测试
    看看效果怎么样  简单就行

---
+++ WingsCMS/Wings.CMS/Controllers/Admin/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wings.CMS.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult login(string username, string password)
+        {
+            if (username==password)
+            {
+                return View("domain");
+            }
+            else
+            {
+                return View("index");
+            }
+        }
+
+        public ActionResult domain()
+        {
+            return View();
+        }
+    }
+}
