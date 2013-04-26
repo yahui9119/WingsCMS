43041827dafaa6de9b18b6ce73fcd619302b9ba5
yhwang <yahui9119@gmail.com>
2013-04-26 20:35:36 +0800

    New Study
    about a demo

---
+++ Wings.CMS/Wings.Resources/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wings.Resources
+{
+    public class Class1
+    {
+    }
+}
---
+++ Wings.CMS/Wings.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wings.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
