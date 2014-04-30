8f12a85b2e0a16b1b0ee412e101a9aa6f3109589
yahui9119 <yahui9119@qq.com>
2014-04-30 18:24:40 +0800

    0

---
+++ 0/WingsCMS/Wings.Framework/IBatisNet/MyBatisNet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IBatisNet.DataMapper.Configuration;
+using IBatisNet.DataAccess;
+using IBatisNet.DataMapper; 
+
+namespace Wings.Framework.IBatisNet
+{
+    public class MyBatisNet
+    {
+    }
+}
